e71965b343bc7135279ac2bc02836a889d0b6eb7
Penjat <theSpencerSymington@Gmail.com>
2019-08-23 10:39:11 -0700

    basic set up

---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour{
+
+    void Update(){
+      if (Input.GetMouseButtonDown(0)){
+        TouchBegan();
+      }
+      if (Input.GetMouseButtonUp(0)){
+        TouchEnded();
+      }
+    }
+
+    private void TouchBegan(){
+      Debug.Log("touch began");
+    }
+    private void TouchMoved(){
+      Debug.Log("touch moved");
+    }
+    private void TouchEnded(){
+      Debug.Log("touch ended");
+    }
+}
---
+++ Assets/Scripts/Managers/MainManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour{
+
+    void Start(){
+      Debug.Log("starting up");
+
+    }
+}
